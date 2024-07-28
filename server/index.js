03528d218f46401e243991983ed3acff10c11545
arijit@2004 <arijitmondal0609@gmail.com>
2024-07-28 13:20:46 +0530

    assfg

--- server/index.js
+++ server/index.js
@@ -9,6 +9,10 @@ dotenv.config();
 const app = express();
 
 app.use(cors());
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+});
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use('/', router);
