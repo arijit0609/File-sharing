50cbd8a10423b54391e82116876a735d14a5c58d
arijit@2004 <arijitmondal0609@gmail.com>
2024-07-28 12:55:58 +0530

    htgvgv

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URI = 'http://localhost:8001';
+const API_URI = 'https://file-sharing-b3ez.onrender.com';
 
 export const uploadFile = async (data) => {
     try {
